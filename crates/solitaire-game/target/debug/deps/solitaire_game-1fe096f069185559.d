e54c1f1af3424cfd44611a45e58906542da39d58
beaconbrigade <beaconbrigade@gmail.com>
2025-12-01 20:38:18 -0700

    apparently target was committed for a while...

--- crates/solitaire-game/target/debug/deps/solitaire_game-1fe096f069185559.d
+++
@@ -1,8 +0,0 @@
-C:\Users\rcullen_s\dev\rust\solitaire-solver\crates\solitaire-game\target\debug\deps\solitaire_game-1fe096f069185559.rmeta: src\lib.rs Cargo.toml
-
-C:\Users\rcullen_s\dev\rust\solitaire-solver\crates\solitaire-game\target\debug\deps\solitaire_game-1fe096f069185559.d: src\lib.rs Cargo.toml
-
-src\lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
